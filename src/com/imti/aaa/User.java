58a6e741b3c8453a7510dec2a67d875722abad4d
dongyufan2 <3045035960@qq.com>
2019-12-24 09:32:58 +0800

    第一次打猪儿虫

---
+++ src/com/imti/aaa/User.java
@@ -0,0 +1,14 @@
+package com.imti.aaa;
+
+public class User {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
